dda9105dd617489d71e183b4839721f36d609ad4
Marcelo Guazzardo <tercermundo19@gmail.com>
2025-04-03 03:38:28 -0300

    saving errors in app4

--- app5.py
+++ app5.py
@@ -186,8 +186,7 @@ with st.sidebar:
         [
             "llama3-8b-8192",
             "llama3-70b-8192",
-            "llama2-70b-4096",
-            "deepseek-coder-33b-instruct"
+            "llama2-70b-4096"
         ]
     )
     
